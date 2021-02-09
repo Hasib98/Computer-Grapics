69af86f6617fa08e292009c762652d9e8ace5a71
Hasib98 <phoenixoffire1998@gmail.com>
2021-02-09 09:15:35 +0600

    circle and filled circle

---
+++ circle/main.cpp
@@ -0,0 +1,67 @@
+#include <windows.h>  // for MS Windows
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include<math.h>>
+# define PI           3.14159265358979323846
+/* Handler for window-repaint event. Call back when the window first appears and
+whenever the window needs to be re-painted. */
+void display1() {
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black and opaque
+	glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer (background)
+
+	GLfloat x=.4f; GLfloat y=.4f; GLfloat radius =.2f;
+	int i;
+	int lineAmount = 100; //# of triangles used to draw circle
+
+	//GLfloat radius = 0.8f; //radius
+	GLfloat twicePi = 2.0f * PI;
+
+	glBegin(GL_LINE_LOOP);
+	//glColor3f(1,0,0);
+	//glColor3ub(204,0,204);
+		for(i = 0; i <= lineAmount;i++) {
+			glVertex2f(
+			    x + (radius * cos(i *  twicePi / lineAmount)),
+			    y + (radius* sin(i * twicePi / lineAmount))
+			);
+		}
+	glEnd();
+
+
+	//.......................
+
+	int j;
+
+	GLfloat x1=-0.4f; GLfloat y1=.4f; GLfloat radius1 =.2f;
+	int triangleAmount1 = 20; //# of triangles used to draw circle
+
+	//GLfloat radius = 0.8f; //radius
+	GLfloat twicePi1 = 2.0f * PI;
+
+	glBegin(GL_TRIANGLE_FAN);
+		glVertex2f(x1, y1); // center of circle
+		for(j = 0; j <= triangleAmount1;j++) {
+			glVertex2f(
+		            x1 + (radius1 * cos(j *  twicePi1 / triangleAmount1)),
+			    y1 + (radius1 * sin(j * twicePi1 / triangleAmount1))
+			);
+		}
+	glEnd();
+
+
+
+	glFlush();  // Render now
+}
+
+
+
+
+
+/* Main function: GLUT runs as a console application starting at main()  */
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);                 // Initialize GLUT
+	glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
+	glutInitWindowSize(320, 320);   // Set the window's initial width & height
+	glutDisplayFunc(display1); // Register display callback handler for window re-paint
+	glutMainLoop();           // Enter the event-processing loop
+	return 0;
+}
